34488ab707e8038c31d6738326868effed375faa
ittirarichan <abhishekbinish>
2024-09-18 11:29:53 +0530

    js

---
+++ sample/alert.js
@@ -0,0 +1,65 @@
+// a='Please wait'
+// alert(a)
+
+
+
+
+// var a
+// a='hello'
+// alert(a)
+
+
+// let a='hai'
+// a='hello'
+// alert(a)
+
+
+
+// const a=44
+// alert(a)
+
+
+// let a=10
+// let a=[1234]
+// let a={1234}
+// let a
+// a=true
+// a='hredfg'
+// alert(typeof a)
+
+
+
+// operators
+
+// arithmetic
+let a=10
+let b=2
+// alert(a+b)
+// alert(a-b)
+// alert(a/b)
+// alert(a*b)
+// a++
+// a--
+// alert(a)
+
+
+// asignment
+// a+=10
+// a-=10
+// a*=10
+// a/=10
+// a%=10
+// alert(a)
+
+// comparison
+// alert(a==b)
+// alert(a!=b)
+// alert(a>=b)
+// alert(a<=b)
+// alert(a<b)
+// alert(a>b)
+
+// logical
+// alert(a==b && a>8)
+// alert(a==b || a>8)
+// alert(! a==b)
