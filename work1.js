bdf43d2c7625baa47d130bd50e81f3f1ba30c293
ittirarichan <abhishekbinish86@gmail.com>
2024-09-25 11:29:36 +0530

    js

--- work1.js
+++ work1.js
@@ -7,7 +7,44 @@ d = [
     { name: "arjun", age: 35, place: "palluruthy" },
   ];
 
+function display(){
+  const t_var=document.querySelector('.tbl tbody')
+  t_var.innerHTML=""
   d.forEach(element => {
-    console.log(element.age);
+    const t_row=document.createElement('tr')
     
+    
+    const t_name=document.createElement('td')
+    t_name.textContent=element.name
+    t_row.appendChild(t_name)
+    
+    const t_age=document.createElement('td')
+    t_age.textContent=element.age
+    t_row.appendChild(t_age)
+    
+    const t_plc=document.createElement('td')
+    t_plc.textContent=element.place
+    t_row.appendChild(t_plc)
+
+    t_var.appendChild(t_row)
   });
+}
+
+document.getElementById("form 1").addEventListener('submit',function(event){
+  event.preventDefault()  //remove refresh
+  const name=document.getElementById('name').value
+  const age=parseInt(document.getElementById('age').value)
+  const place=document.getElementById('plc').value
+
+  d.push({name:name,age:age,place:place})
+
+  document.getElementById('name').value=''
+  document.getElementById('age').value=''
+  document.getElementById('plc').value=''
+
+  display()
+})
+
+
+
+display()
