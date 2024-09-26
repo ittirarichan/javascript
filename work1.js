b08fc36252ff61de50168c8204c10c4343495024
ittirarichan <abhishekbinish86@gmail.com>
2024-09-26 11:29:34 +0530

    js

--- work1.js
+++ work1.js
@@ -26,11 +26,21 @@ function display(){
     t_plc.textContent=element.place
     t_row.appendChild(t_plc)
 
+const edit_td=document.createElement('td')
+const edit_btn=document.createElement('button')
+edit_btn.textContent="edit"
+edit_btn.onclick=function(){
+  edit_frm(element.name)
+}
+
+    edit_td.appendChild(edit_btn)
+    t_row.appendChild(edit_td)
+
     t_var.appendChild(t_row)
   });
 }
 
-document.getElementById("form 1").addEventListener('submit',function(event){
+document.getElementById("submit_form").addEventListener('submit',function(event){
   event.preventDefault()  //remove refresh
   const name=document.getElementById('name').value
   const age=parseInt(document.getElementById('age').value)
@@ -45,6 +55,34 @@ document.getElementById("form 1").addEventListener('submit',function(event){
   display()
 })
 
+let edit_name=''
+function edit_frm(name) {
+  console.log('editing',name);
+  document.getElementById('edit_form').style.display='block'
+  document.getElementById('submit_form').style.display='none'
+  const edit_data=d.find(user=>user.name==name)
+  document.getElementById('e_name').value=edit_data.name
+  document.getElementById('e_age').value=edit_data.age
+  document.getElementById('e_plc').value=edit_data.place
+  edit_name=name
+}
 
+document.getElementById('edit_form').addEventListener('submit',function(event){
+  event.preventDefault()
+  const e_name=document.getElementById('e_name').value
+  const e_age=document.getElementById('e_age').value
+  const e_plc=document.getElementById('e_plc').value
+  // console.log(e_name,e_age,e_plc);
+  d=d.map(user=>{
+   if(user.name==edit_name){
+      return{...user,name:e_name,age:e_age,place:e_plc}
+    }
+  return user 
+  })
+
+document.getElementById('edit_form').style.display='none'
+document.getElementById('submit_form').style.display='block'
+display()
+})
 
 display()
