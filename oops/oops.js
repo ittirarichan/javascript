5b51e4baf7f24e2dae74d270f154a8ae6e649641
ittirarichan <abhishekbinish86@gmail.com>
2024-10-03 11:27:37 +0530

    api

---
+++ oops/oops.js
@@ -0,0 +1,34 @@
+// class Car {
+//     constructor(name,year) {
+//         this.name=name
+//         this.year=year
+//     }
+//     display(){
+//         console.log(this.name,this.year);
+//     }
+// }
+
+// bmw=new Car('bmw',2022)
+// bmw.display()
+
+
+
+
+// d={
+//     f_name:'Abhishek ',
+//     l_name:'Binish',
+//     age:21,
+//     full_name:function(){
+//         return this .f_name+this.l_name
+//     }
+// }
+// console.log(d.full_name());
+
+
+
+try{
+    alert('welcome')
+} catch(error){
+    console.log('error');
+    
+}
--- timer/timer.js
+++ timer/timer.js
@@ -1,19 +1,24 @@
 function demo(){
-            const main=document.querySelector('.main')
-            const div=document.createElement('div') 
-            let count1=3
-            let count=60
-            let c=setInterval(function(){
-
-                div.innerHTML=`<h1 style="text-align:center; color:red">00: ${count}</h1>`   
-                count--
-                if(count==0){
-                    clearInterval(c)
-                    alert("Completed")
-                    div.innerHTML=`<h1 style="text-align:center; color:red">Completed</h1>`   
-
-                }
-            },1000)
-            main.appendChild(div)
+    const main = document.querySelector('.main');
+    const div = document.createElement('div'); 
+    let count1 = 2; 
+    let count = 59; 
+    let c = setInterval(function(){
+        div.innerHTML = `<h1 style="text-align:center; color:red">0${count1}:${count < 10 ? '0' + count : count}</h1>`;   
+        count--;
+        
+        if (count == 0) {
+            count1--;  
+            count = 59;  
         }
-        demo()
+        
+        if (count1 == 0) {
+            clearInterval(c);
+            alert("Completed");
+            div.innerHTML = `<h1 style="text-align:center; color:red">Completed</h1>`;
+        }
+    }, 1000);
+    
+    main.appendChild(div);
+}
+demo();
